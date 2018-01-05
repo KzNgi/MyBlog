54363c873e555b4b4c7afdeb731947421b74d683
Kzk Nagai <ng.mei.sha@gmail.com>
2018-01-05 00:20:53 +0000

    Add root

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  root 'messages#index'
   resources :messages
   resources :messaves
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
